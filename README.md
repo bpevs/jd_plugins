009fdfd97cde31691850ef52039f9517031177dd
ivebencrazy <github@bpev.me>
2022-03-22 16:03:14 -0700

    Add mod.ts

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 My personal plugins that I use with
 [Johnny Decimal CLI](https://github.com/ivebencrazy/johnny_decimal)
+
+## Usage
+
+Utilities are not published on deno.land.  Import them via github.
+
+```js
+import { getDailyNote } from "https://raw.githubusercontent.com/ivebencrazy/jd_plugins/main/mod.ts";
+```
+
---
+++ mod.ts
@@ -0,0 +1,8 @@
+// Plugins
+export { default as jdPluginDailyNote } from "./jd_plugin_daily_note/main.ts";
+export { default as jdPluginOpen } from "./jd_plugin_open/main.ts";
+export { default as jdPluginSublime } from "./jd_plugin_sublime/main.ts";
+
+// Utilities
+export { default as getDailyNote } from "./shared/get_daily_note_dir.ts";
+export { default as openInSublime } from "./shared/open_in_sublime.ts";
